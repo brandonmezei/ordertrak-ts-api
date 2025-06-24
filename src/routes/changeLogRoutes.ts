8830141df2430c0e6fc9b719efda7e582b885bca
Brandon <brandon.mezei@me.com>
2025-06-23 20:01:20 -0400

    Converted changelog/create just into a post to changelog

--- src/routes/changeLogRoutes.ts
+++ src/routes/changeLogRoutes.ts
@@ -5,7 +5,7 @@ import { authenticate } from "../middleware/authMiddleware";
 const router = Router();
 
 // POST /api/changelog
-router.post('/create', authenticate, createChangeLog);
+router.post('/', authenticate, createChangeLog);
 router.get('/', authenticate, getChangeLogs);
 
 export default router;
